114171555a5f8b0cf487a0fd8c2c7271281b5276
Lsilva-sun <lsilva22@students.avc.edu>
2025-04-26 00:31:13 -0700

    Late commit but fixed

--- main.hpp
+++ main.hpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
-int main()
+int writeFile(string filename);
+int readFile(string filename);
+
+int writeFile(string filename)
+
 {
-
-    int num;
-    int ID;
-    string name; 
-    string department; 
-    double salary; 
-
+    int num, ID;
+    string name, department;
+    double salary;
+    
     cout << "Enter number of employess: ";
     cin >> num;
 
-    ofstream ofs;
-    ofs.open("employee.txt");
+    ofstream ofs(filename);
     if (!ofs)
     {
         cout << "File Open Error\n";
@@ -31,9 +32,39 @@ int main()
     cout << "Enter Salary: " << endl; 
     cin >> salary;
 
-    ofs << i + 1 << num << " " << ID << " " << name << " " << department << " "<< salary << "/n"; 
+    ofs << ID << " " << name << " " << department << " "<< salary << "\n"; 
 
     }
 
     ofs.close();
+    return num; 
+}
+
+int readFile(string filename) {
+
+    int ID;
+    string name, department;
+    double salary;
+    double total, avg;
+    int num = 0;
+
+    ifstream ifs(filename);
+    if (!ifs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+
+    while (ifs >> ID >> name >> department >> salary) {   // no reading num here
+        cout << " Employee ID: " << ID << " Name: " << name << " Department: " << department << " Salary: " << salary << endl;
+        total = total + salary;
+        num++;
+    }
+
+    if (num > 0) {
+        avg = total / num;
+        cout << "\nTotal Salary: " << total
+             << "\nAverage Salary: " << avg << endl;
+    }
+    return num; 
 }
