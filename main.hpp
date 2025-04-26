6f2cf24c597f97cd8e40893e9e2001d95b4e261a
Lsilva-sun <lsilva22@students.avc.edu>
2025-04-25 23:49:12 -0700

    quiz question 2

--- main.hpp
+++ main.hpp
@@ -1,3 +1,39 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+    int num;
+    int ID;
+    string name; 
+    string department; 
+    double salary; 
+
+    cout << "Enter number of employess: ";
+    cin >> num;
+
+    ofstream ofs;
+    ofs.open("employee.txt");
+    if (!ofs)
+    {
+        cout << "File Open Error\n";
+        exit(0);
+    }
+    for (int i = 0; i < num; i++) {
+    cout << "Enter Employee ID: " <<endl;
+    cin >> ID;
+    cout << "Enter Employee Name: " << endl; 
+    cin >> name;
+    cout << "Enter Dempartment: " << endl;
+    cin >> department;
+    cout << "Enter Salary: " << endl; 
+    cin >> salary;
+
+    ofs << i + 1 << num << " " << ID << " " << name << " " << department << " "<< salary << "/n"; 
+
+    }
+
+    ofs.close();
+}
